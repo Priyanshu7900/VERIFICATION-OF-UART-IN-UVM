5ef3c63ed8cd84c3153c8581c7b29569e01d77fd
Priyanshu7900 <164048870+Priyanshu7900@users.noreply.github.com>
2025-08-13 22:22:50 +0530

    Update uart_top.sv

--- uart_top.sv
+++ uart_top.sv
@@ -20,3 +20,15 @@ uart_tx tx_dut (tx_clk,tx_start, rst, tx_data, length, parity_type, parity_en, s
 uart_rx rx_dut (rx_clk, rx_start, rst, tx_rx, length,parity_type, parity_en, stop2,rx_out,rx_done, rx_err);
 
  endmodule
+
+interface uart_if;
+ logic clk, rst;
+ logic tx_start, rx_start;
+ logic [7:0] tx_data;
+ logic [16:0] baud;
+ logic [3:0] length;
+ logic parity_type,parity_en;
+ logic stop2;
+ logic tx_done,rx_done,tx_err,rx_err;
+ logic [7:0]rx_out;
+endinterface
